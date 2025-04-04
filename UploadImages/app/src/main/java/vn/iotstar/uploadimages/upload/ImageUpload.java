8e4fcc01d92c287f90c531daa3896b05a0718311
nguyenvanbao <nguyenhoanggiaphong12@gmail.com>
2025-04-04 09:58:24 +0700

    SocketConnetion + UploadImages

---
+++ UploadImages/app/src/main/java/vn/iotstar/uploadimages/upload/ImageUpload.java
@@ -0,0 +1,15 @@
+package vn.iotstar.uploadimages.upload;
+
+public class ImageUpload {
+    private int id;
+    private String username;
+    private String avatar;
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getAvatar() { return avatar; }
+    public void setAvatar(String avatar) { this.avatar = avatar; }
+}
